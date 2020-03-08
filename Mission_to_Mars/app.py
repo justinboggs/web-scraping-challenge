8a2e7a8580c22a8515d8050ceeba9faaa363845d
Justin Boggs <jboggs1@gmail.com>
2020-03-07 23:58:30 -0600

    updates

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -6,17 +6,33 @@ import os
 # create instance of Flask app
 app = Flask(__name__)
 
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_scraper"
+mongo = PyMongo(app)
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
 
-    # Find one record of data from the mongo database
-    destination_data = mongo.db.collection.find_one()
+    mars_data = mongo.db.mars_data.find_one()
 
     # Return template and data
-    return render_template("index.html", vacation=destination_data)
+    return render_template("index.html", mars_data=mars_data)
 
+# Route that will trigger the scrape function
+@app.route("/scrape")
+def scrape():
+
+    # Run the scrape function
+    mars_data = mongo.db.mars_data
+    mars_data2 = scrape_mars.scrape_news()
+    mars_data2 = scrape_mars.scrape_image()
+    # Update the Mongo database using update and upsert=True
+    mars_data.update({}, mars_data2, upsert=True)
+
+    # Update the Mongo database using update and upsert=True
+
+    # Redirect back to home page
+    return redirect("/" code=302)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -7,14 +7,16 @@ import requests
 
 def init_browser():
     executable_path = {"executable_path": "chromedriver"}
-    return Browser("chrome", **executable_path, headless=False)
+    return Browser("chrome", **executable_path, headless=True)
 
 mars_data = {}
 
 def scrape_news():
+    
+    browser = init_browser()
+
     news_url = 'https://mars.nasa.gov/news/'
     browser.visit(news_url)
-
     time.sleep(3)
 
     html_news = browser.html
@@ -29,3 +31,27 @@ def scrape_news():
     return mars_data
 
     browser.quit()
+
+def scrape_image():
+
+    browser = init.browser()
+
+    space_image_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(space_image_url)
+    time.sleep(3)
+
+    html_image = browser.html
+    soup = BeautifulSoup(html_image, 'html.parser')
+
+    featured_image_url = soup.find('article')['style'].replace('background-image: url(','').replace(');','')[1:-1]
+
+    nasa_main_url = 'https://www.jpl.nasa.gov'
+
+    featured_image_url = nasa_main_url + featured_image_url
+
+    mars_data['featured_image_url'] = featured_image_url
+
+    return mars_data
+
+    browser.quit()
+
