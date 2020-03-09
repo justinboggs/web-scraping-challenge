667c0248cf48676b0d5f85e47a48378265e97034
Justin Boggs <jboggs1@gmail.com>
2020-03-09 13:16:40 -0500

    updates

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -12,7 +12,7 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_scraper")
 @app.route("/")
 def home():
 
-    mars_data = mongo.db.mars_collection.find_one()
+    mars_data = mongo.db.collection.find_one()
 
     # Return template and data
     return render_template("index.html", mars_scrape=mars_data)
@@ -22,10 +22,10 @@ def home():
 def scrape():
 
     # Run the scrape function
-    mars_final = scrape_mars.scrape
+    mars_final = scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.mars_collection.update({}, mars_final, upsert=True)
+    mongo.db.collection.update({}, mars_final, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
